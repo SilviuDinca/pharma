2daf5126ac985ea1f04b68c03a48a5ef7f5814e5
Silviu Dinca <silviu_dinca@yahoo.com>
2023-03-10 02:44:08 +0200

    feat: add links bootstrap, add navigation bar, add image-fluid in index.html and get medication in dashboardClient.js

--- client/js/dashboardClient.js
+++ client/js/dashboardClient.js
@@ -1,2 +1,9 @@
 'use strict'
+import { axios } from "axios"
 
+async function getMedication() {
+    let res = await axios.get('http://localhost:8000/api/medication/')
+    let data = res;
+    console.log(data)
+    }
+    getMedication()
