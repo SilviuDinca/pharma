c7af8a2d84eea59e920023c62508f576154548cf
Silviu Dinca <silviu_dinca@yahoo.com>
2023-03-12 19:51:54 +0200

    feat: add medicines

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const db = require("../db/database.js")
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
 const PORT = 8000;
@@ -11,6 +12,31 @@ app.listen(PORT, () => {
 })
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/medication', (req,res,next) => {
+    let data = {
+      name: req.body.name,
+      dose: req.body.dose,
+      description : req.body.description,
+      expirationDate: req.body.expirationDate,
+      pieces: req.body.pieces
+  }
+  let sql ='INSERT INTO medication (name, dose, description, expirationDate, pieces) VALUES (?,?,?,?,?)'
+      let params =[data.name, data.dose, data.description, data.expirationDate, data.pieces]
+      db.run(sql, params, function (err, result) {
+          if (err){
+              res.status(400).json({"error": err.message})
+              return;
+          }
+          res.json({
+              "message": "success",
+              "data": data,
+              "id" : this.lastID
+          })
+      })
+  })
 
 app.get("/api/medication", (req, res, next) => {
     const sql = "select * from medication"
@@ -26,3 +52,4 @@ app.get("/api/medication", (req, res, next) => {
         })
       });
 });
+
