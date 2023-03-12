2edd9389178132bf14e03fac1b22ea542d9fa7d9
Silviu Dinca <silviu_dinca@yahoo.com>
2023-03-12 19:54:44 +0200

    feat: add medicines

--- client/js/dashboardClient.js
+++ client/js/dashboardClient.js
@@ -1,6 +1,64 @@
+"use strict";
+
+let card = document.querySelector('.section')
+const cardBody = document.querySelector('.medicine')
+const inputName = document.querySelector('.input-name')
+const inputDose = document.querySelector('.input-dose')
+const inputDescription = document.querySelector('.input-description')
+const inputExpiration = document.querySelector('.input-expiration')
+const inputPieces = document.querySelector('.input-pieces')
+const addMedicine = document.querySelector('.btn-submit')
+
+
+ async function addMedication() {
+  const createMedication = await axios.post("http://localhost:8000/api/medication/", {
+    name:inputName.value,
+    dose:inputDose.value,
+    description:inputDescription.value,
+    expirationDate:inputExpiration.value,
+    pieces:inputPieces.value
+  })
+  console.log(createMedication)
+  
+}
+
+const update = () => {
+addMedicine.addEventListener('click', addMedication)
+}
+
 async function getMedication() {
-    let res = await axios.get('http://localhost:8000/api/medication/')
-    let data = res;
-    console.log(data)
-    }
-    getMedication()
+  let res = await axios.get("http://localhost:8000/api/medication/");
+  let data = res;
+  showMedicine(data.data.data)
+  update()
+}
+getMedication();
+
+const showMedicine = (data) => {
+    card = ''
+    data.forEach((item) => {
+        card += `
+        <div class="card col border-primary" style="width: 18rem">
+        <div class="card-body">
+        <div class="d-flex justify-content-between align-items-center">
+              <h5 class="card-title">${item.name}</h5>
+              <div>
+                <button type="button" class="btn btn-outline-success"><i class="bi bi-pencil"></i></button>
+                <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash3"></i></button>
+              </div>
+            </div>
+            <h6 class="card-subtitle mb-2 text-muted">${item.dose} mg</h6>
+            <p class="card-text">
+              ${item.description}
+            </p>
+            <div class="d-flex justify-content-between">
+              <p>${item.expirationDate}</p>
+              <p>Nr. buc: ${item.pieces}</p>
+            </div>
+          </div>
+          </div>
+        `
+    })
+    cardBody.innerHTML = card
+    return cardBody
+}
--- db/database.js
+++ db/database.js
@@ -1,6 +1,5 @@
-'use strict'
+let sqlite3 = require('sqlite3').verbose()
 
-const sqlite3 = require('sqlite3').verbose()
 const DBSOURCE = "db.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
@@ -13,7 +12,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         db.run(`CREATE TABLE medication (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text, 
-            weight text, 
+            dose text, 
             description text, 
             expirationDate text,
             pieces num
@@ -23,12 +22,11 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 // Table already created
             }else{
                 // Table just created, creating some rows
-                const insert = 'INSERT INTO medication (name, weight, description, expirationDate, pieces) VALUES (?,?,?,?,?)'
-                db.run(insert, ["Aspirina","20g", 'pastila', '12/02/2020', 5])
-                db.run(insert, ["Nurofen","20g", 'pastila', '12/12/2025', 15])
-                db.run(insert, ["Amoxicilina","20g", 'pastila', '12/12/2025', 35])
-                db.run(insert, ["Strepsils","20g", 'pastila', '12/12/2025', 45])
-
+                let insert = 'INSERT INTO medication (name, dose, description, expirationDate, pieces) VALUES (?,?,?,?,?)'
+                db.run(insert, ["Aspirina","20", "Aspirina, sau acidul acetilsalicilic, este un medicament antiinflamator non-steroidian din familia salicilaților, folosit în general ca un analgezic minor, ca antipiretic, sau ca antiinflamator. În plus, aspirina în doze mici are un efect antiagregant și este folosit pe termen lung ca să diminueze riscul de infarct.","12/02/2025",5])
+                db.run(insert, ["Nurofen","400","Nurofen Forte 400 mg drajeuri calmează eficace un spectru larg de dureri acute. Utilizare: 1 drajeu administrat într-o priză unică sau de 3 ori pe zi, la intervale de 4 până la 6 ore. În migrenă, dozajul trebuie să fie: 1 drajeu administrat într-o priză unică, dacă este necesar 1 drajeu la intervale de 4 până la 6 ore.","12/06/2025", 5])
+                db.run(insert, ["Amoxicilina","25","Amoxicilina este un antibiotic din clasa beta-lactamelor, aparținând grupului penicilinelor (amino-peniciline) de semi-sinteză cu spectru lărgit. Are efect bactericid, acționând la nivelul peretelui celulei bacteriene prin inhibarea biosintezei substanțelor din structura peretelui.","12/12/2025", 5])
+                db.run(insert, ["Strepsils","20", "Strepsils Intensiv contine flurbiprofen care apartine unui grup de substante numite antiinflamatoare nesteroidiene (Ains). Ains ajuta la reducerea inflamatiei, a durerii si febrei. Pastilele Strepsils Intensiv Miere si Lamaie calmeaza durerea si inflamatia gatului ce insotesc raceala, laringita sau amigdalita.","12/01/2025",5])
             }
         });  
     }
