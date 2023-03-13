a7855ae0e414684f7fbbaed2761f073117e0c310
Silviu Dinca <silviu_dinca@yahoo.com>
2023-03-13 20:36:30 +0200

    add call api for delete and update in comments

--- client/js/dashboardClient.js
+++ client/js/dashboardClient.js
@@ -1,50 +1,84 @@
 "use strict";
 
-let card = document.querySelector('.section')
-const cardBody = document.querySelector('.medicine')
-const inputName = document.querySelector('.input-name')
-const inputDose = document.querySelector('.input-dose')
-const inputDescription = document.querySelector('.input-description')
-const inputExpiration = document.querySelector('.input-expiration')
-const inputPieces = document.querySelector('.input-pieces')
-const addMedicine = document.querySelector('.btn-submit')
+let card = document.querySelector(".section");
+const cardBody = document.querySelector(".medicine");
+const inputName = document.querySelector(".input-name");
+const inputDose = document.querySelector(".input-dose");
+const inputDescription = document.querySelector(".input-description");
+const inputExpiration = document.querySelector(".input-expiration");
+const inputPieces = document.querySelector(".input-pieces");
+const addMedicine = document.querySelector(".btn-submit");
 
-
- async function addMedication() {
-  const createMedication = await axios.post("http://localhost:8000/api/medication/", {
-    name:inputName.value,
-    dose:inputDose.value,
-    description:inputDescription.value,
-    expirationDate:inputExpiration.value,
-    pieces:inputPieces.value
-  })
-  console.log(createMedication)
-  
-}
-
-const update = () => {
-addMedicine.addEventListener('click', addMedication)
-}
+// const deleteMedication = async (id) => {
+//   let res = await axios.delete(`http://localhost:8000/api/medication/${id}`);
+//   console.log(res)
+// };
 
 async function getMedication() {
   let res = await axios.get("http://localhost:8000/api/medication/");
   let data = res;
-  showMedicine(data.data.data)
-  update()
+  console.log(data.data.data);
+  showMedicine(data.data.data);
+  // const btnSave = document.querySelector(".btn-save");
+  // btnSave.addEventListener("click", updateMedication);
+  addMedicine.addEventListener("click", addMedication);
+
 }
 getMedication();
 
+// async function updateMedication() {
+//   let res = await axios.put("http://localhost:8000/api/medication/");
+//   console.log(res);
+// }
+
 const showMedicine = (data) => {
-    card = ''
-    data.forEach((item) => {
-        card += `
-        <div class="card col border-primary" style="width: 18rem">
+  card = "";
+
+  data.forEach((item) => {
+    card += `
+        <div class="card-id card col border-primary" data-index="${item.id}" style="width: 18rem">
         <div class="card-body">
         <div class="d-flex justify-content-between align-items-center">
               <h5 class="card-title">${item.name}</h5>
               <div>
-                <button type="button" class="btn btn-outline-success"><i class="bi bi-pencil"></i></button>
-                <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash3"></i></button>
+                <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-pencil"></i></button>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Edit the medication</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+          <div class="modal-body">
+          <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInput" value="${item.name}">
+          <label for="floatingInput">Name</label>
+        </div>
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingPassword" value="${item.dose}">
+          <label for="floatingPassword">Dose</label>
+        </div>
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingPassword" value="${item.description}">
+          <label for="floatingPassword">Description</label>
+        </div>
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingPassword" value="${item.expirationDate}">
+          <label for="floatingPassword">Expiration Date</label>
+        </div>
+        <div class="form-floating">
+          <input type="number" class="form-control" id="floatingPassword" value="${item.pieces}">
+          <label for="floatingPassword">Pieces</label>
+        </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn-save btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+                <button type="button" class="btn btn-outline-danger btn-del" data-index="${item.id}"><i class="bi bi-trash3"></i></button>
               </div>
             </div>
             <h6 class="card-subtitle mb-2 text-muted">${item.dose} mg</h6>
@@ -57,8 +91,35 @@ const showMedicine = (data) => {
             </div>
           </div>
           </div>
-        `
-    })
-    cardBody.innerHTML = card
-    return cardBody
+        `;
+    console.log(item.id);
+  });
+  cardBody.innerHTML = card;
+  // const btnDelete = document.querySelectorAll(".btn-del");
+  // btnDelete.forEach((btn) => {
+  //   console.log(btn.dataset.index)
+  //   if(btn.dataset.index === card1()) {
+  //     btn.addEventListener("click", deleteMedication);
+  //   }
+  // });
+};
+
+// const card1 = () => {
+//   const cardId = document.querySelectorAll('.card-id')
+//   cardId.forEach((item) => item.dataset.index)
+// }
+
+async function addMedication() {
+  const createMedication = await axios.post(
+    "http://localhost:8000/api/medication/",
+    {
+      name: inputName.value,
+      dose: inputDose.value,
+      description: inputDescription.value,
+      expirationDate: inputExpiration.value,
+      pieces: inputPieces.value,
+    }
+  );
+  const data = createMedication.data.data;
+  return data;
 }
--- server/server.js
+++ server/server.js
@@ -1,55 +1,100 @@
-const express = require('express')
-const app = express()
-const db = require("../db/database.js")
-const cors = require('cors')
-const bodyParser = require('body-parser')
-
+const express = require("express");
+const app = express();
+const db = require("../db/database.js");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
-})
+  console.log(`Server running on ${PORT}`);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/medication', (req,res,next) => {
-    let data = {
-      name: req.body.name,
-      dose: req.body.dose,
-      description : req.body.description,
-      expirationDate: req.body.expirationDate,
-      pieces: req.body.pieces
-  }
-  let sql ='INSERT INTO medication (name, dose, description, expirationDate, pieces) VALUES (?,?,?,?,?)'
-      let params =[data.name, data.dose, data.description, data.expirationDate, data.pieces]
-      db.run(sql, params, function (err, result) {
-          if (err){
-              res.status(400).json({"error": err.message})
-              return;
-          }
-          res.json({
-              "message": "success",
-              "data": data,
-              "id" : this.lastID
-          })
-      })
-  })
-
-app.get("/api/medication", (req, res, next) => {
-    const sql = "select * from medication"
-    const params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
+app.post("/api/medication", (req, res, next) => {
+  let data = {
+    name: req.body.name,
+    dose: req.body.dose,
+    description: req.body.description,
+    expirationDate: req.body.expirationDate,
+    pieces: req.body.pieces,
+  };
+  let sql =
+    "INSERT INTO medication (name, dose, description, expirationDate, pieces) VALUES (?,?,?,?,?)";
+  let params = [
+    data.name,
+    data.dose,
+    data.description,
+    data.expirationDate,
+    data.pieces,
+  ];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data,
+      id: this.lastID,
+    });
+  });
 });
 
+app.get("/api/medication", (req, res, next) => {
+  const sql = "select * from medication";
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+// app.patch("/api/medication/:id", (req, res, next) => {
+//   let data = {
+//       name: req.body.name,
+//       dose: req.body.dose,
+//       description: req.body.description,
+//       expirationDate: req.body.expirationDate,
+//       pieces: req.body.pieces
+//   }
+//   db.run(
+//       `UPDATE medication set 
+//          name = COALESCE(?,name), 
+//          dose = COALESCE(?,dose), 
+//          description = COALESCE(?,description),
+//          expirationDate = COALESCE(?,expirationDate),
+//          pieces = COALESCE(?,pieces)
+//          WHERE id = ?`,
+//       [data.name, data.dose, data.description,data.expirationDate,data.pieces, req.params.id],
+//       function (err, result) {
+//           if (err){
+//               res.status(400).json({"error": res.message})
+//               return;
+//           }
+//           res.json({
+//               message: "success",
+//               data: data,
+//               changes: this.changes
+//           })
+//   });
+// })
+
+// app.delete("/api/medication/:id", (req, res, next) => {
+//   db.run("DELETE FROM medication WHERE id = ?", req.params.id, function (err, result) {
+//     if (err) {
+//       res.status(400).json({ error: res.message });
+//       return;
+//     }
+//     res.json({ message: "deleted", changes: this.changes });
+//   });
+// });
