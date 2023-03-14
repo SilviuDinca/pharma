da7985d0c9460b63e7cc0538b0be7e671e8744a8
Silviu Dinca <silviu_dinca@yahoo.com>
2023-03-14 21:37:34 +0200

    feat: add in forms for all inputs, validation input

--- client/js/dashboardClient.js
+++ client/js/dashboardClient.js
@@ -14,25 +14,38 @@ const showSignIn = document.querySelector(".show-signIn");
 const hiddenSignUp = document.querySelector(".hidden-signUp");
 const imageContainer = document.querySelector(".image-container");
 const containerSection = document.querySelector(".container-section");
+const inputs = document.querySelectorAll(".inputs");
+const signInUsername = document.getElementById("signInUsername");
+const signInPassword = document.getElementById("signInPassword");
+const inputUsername = document.getElementById("inputUsername");
+const inputPassword = document.getElementById("inputPassword");
+const inputAddress = document.getElementById("inputAddress");
+const inputCNP = document.getElementById("inputCNP");
+const inputCity = document.getElementById("inputCity");
+const inputPhone = document.getElementById("inputPhone");
+const inputPharmaceutist = document.getElementById("pharmaceutistRadio");
+const inputPatient = document.getElementById("patientRadio");
+const wrongInput = document.querySelector(".wrong-input");
+const invalidGroupInput = document.querySelectorAll(".invalid-group-input");
 
 async function signUp() {
   const createUser = {
-    username: document.getElementById("inputUsername").value,
-    password: document.getElementById("inputPassword").value,
-    address: document.getElementById("inputAddress").value,
-    cnp: document.getElementById("inputCNP").value,
-    city: document.getElementById("inputCity").value,
-    phone: document.getElementById("inputPhone").value,
-    pharmaceutist: document.getElementById("pharmaceutistRadio").checked,
-    patient: document.getElementById("patientRadio").checked,
+    username: inputUsername.value,
+    password: inputPassword.value,
+    address: inputAddress.value,
+    cnp: inputCNP.value,
+    city: inputCity.value,
+    phone: inputPhone.value,
+    pharmaceutist: inputPharmaceutist.checked,
+    patient: inputPatient.checked,
   };
   const res = await axios.post("http://localhost:8000/api/user", createUser);
 }
 
 async function signIn() {
   const createUser = {
-    username: document.getElementById("signInUsername").value,
-    password: document.getElementById("signInPassword").value,
+    username: signInUsername.value,
+    password: signInPassword.value,
   };
   const res = await axios.post("http://localhost:8000/api/login", createUser);
   if (res.data.data[0]) {
@@ -42,17 +55,37 @@ async function signIn() {
     containerSection.classList.remove("d-none");
     imageContainer.classList.add("d-block");
     containerSection.classList.add("d-grid");
+  } else {
+    wrongInput.classList.add("invalid-feedback");
+    wrongInput.classList.add("validation");
+    wrongInput.classList.add("d-grid");
+    wrongInput.textContent = "Wrong username or password! Try again!!";
+    signInUsername.value = "";
+    signInPassword.value = "";
   }
 }
 
 btnSignIn.addEventListener("click", signIn);
 
 btnSignUp.addEventListener("click", () => {
-  signUp();
-  hiddenSignUp.classList.remove("d-grid");
-  hiddenSignUp.classList.add("d-none");
-  showSignIn.classList.remove("d-none");
-  showSignIn.classList.add("d-grid");
+  if (
+    inputUsername.value === "" ||
+    inputPassword.value === "" ||
+    inputAddress.value === "" ||
+    inputCNP.value === "" ||
+    inputCity.value === "" ||
+    inputPhone.value === ""
+  ) {
+    invalidGroupInput.forEach((item) => {
+      item.classList.add("d-grid");
+    });
+  } else {
+    signUp();
+    hiddenSignUp.classList.remove("d-grid");
+    hiddenSignUp.classList.add("d-none");
+    showSignIn.classList.remove("d-none");
+    showSignIn.classList.add("d-grid");
+  }
 });
 
 async function deleteMedication(id) {
@@ -65,7 +98,22 @@ async function deleteMedication(id) {
 async function getMedication() {
   const res = await axios.get("http://localhost:8000/api/medication");
   showMedicine(res.data.data);
-  addMedicine.addEventListener("click", addMedication);
+  addMedicine.addEventListener("click", () => {
+    if (
+      inputName.value === "" ||
+      inputDose.value === "" ||
+      inputDescription.value === "" ||
+      inputExpiration.value === "" ||
+      inputPieces.value === ""
+    ) {
+      invalidGroupInput.forEach((item) => {
+        item.classList.add("d-grid");
+      });
+    } else {
+      addMedication();
+      inputs.forEach((input) => (input.value = ""));
+    }
+  });
 }
 
 async function updateMedication() {
